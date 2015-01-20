73c6be43db5613d519074f021629e66392d0ba36
Jonathan Lam <jonathanryanlam@gmail.com>
2015-01-20 17:26:03 -0500

    Method to output list once exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,13 @@
 grocery_list = ["fruits", "milk", "juice", "chips"];
+def item(i)
+	i.each {|x| puts "* #{x}"}
+end
+#METHOD
 
-grocery_list.each {|x| puts "* #{x}"}
 
 
+grocery_list.push('rice')
+item(grocery_list)
+
+
+# puts grocery_list

d8773497e30e85f5699209790cb46be3fc602acb
Jonathan Lam <jonathanryanlam@gmail.com>
2015-01-20 17:33:02 -0500

    bananas

--- exercise6.rb
+++ exercise6.rb
@@ -5,10 +5,16 @@ end
 #METHOD
 
 
-
+grocery_list.push('bananas')
 grocery_list.push('rice')
 item(grocery_list)
 
 
 # puts grocery_list
 puts "the number of items your grocery list is " + grocery_list.count.to_s
+
+if grocery_list.include?('bananas')
+	puts "you need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
+end
